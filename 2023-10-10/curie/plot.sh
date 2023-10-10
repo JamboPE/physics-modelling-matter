6929c660f2847a239ce8e962f5c2555e0063b75f
JamboPE <github@jdpe.co.uk>
2023-10-10 11:01:53 +0100

    Added monte carlo files and plotting scripts

---
+++ 2023-10-10/curie/plot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+plot "output" using 1:2 with points notitle
+set xlabel "Temperature (K)"
+set ylabel "Mean magnetisation (|m|)"
+m(T) = (1-T/Tc)**beta
+Tc = 500.0
+beta=0.3333
+fit m(x) "output" u 1:2 via Tc, beta
+m(T) = T<Tc ? (1-T/Tc)**beta : 0.0
+p "output" u 1:2 w lp, m(x) w l
