a7015bdcbdec67362ef34e7fc412246f4f25685f
JamboPE <github@jdpe.co.uk>
2023-10-10 11:11:33 +0100

    Added script to plot based on config file

--- 2023-10-10/curie/plot.sh
+++ 2023-10-10/curie/plot.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-plot "output" using 1:2 with points notitle
-set xlabel "Temperature (K)"
-set ylabel "Mean magnetisation (|m|)"
-m(T) = (1-T/Tc)**beta
-Tc = 500.0
-beta=0.3333
-fit m(x) "output" u 1:2 via Tc, beta
-m(T) = T<Tc ? (1-T/Tc)**beta : 0.0
-p "output" u 1:2 w lp, m(x) w l
+echo "Plotting graph with the following parameters:"
+cat plotting_parameters.gnu
+gnuplot plotting_parameters.gnu
